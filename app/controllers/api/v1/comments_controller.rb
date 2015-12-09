7163e544b13828821f182c22cf3072437a257bf4
Sia <siakaramalegos@gmail.com>
2015-12-09 09:49:10 -0600

    Updates the index method to return products with comment count and url

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::CommentsController < ApplicationController
   def index
+    products = HTTParty.get('http://localhost:3000/api/v1/products')
+    products.each do |product|
+      product[:comments_count] = Comment.where(product_id: product["id"]).count
+      product[:local_url] = 'http://localhost:9000/api/v1/comments/' + product["id"].to_s
+    end
+    @products = products
   end
 
   def show
